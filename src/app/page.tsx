ce40285480bde2df110489d47cb49d290a95a68e
Empyrean25 <bjorn@empyrean.ph>
2025-07-14 21:49:17 +0800

    Update page.tsx
    changed description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { Metadata } from "next";
 import HomeClient from "./HomeClient";
 
 export const metadata: Metadata = {
-  title: "Home | Empyrean - Your Trusted Partner in Business Solutions",
+  title: "Home | Empyrean Real Estate Solutions - Your all-in-one access to the Philippine Real Estate",
   description:
-    "Welcome to Empyrean. We provide innovative business solutions and services to help your company grow. Expert consulting, digital transformation, and strategic planning services.",
+    "Welcome to Empyrean Real Estate Solutions - We are a one-stop shop for all your Real Estate needs.",
 };
 
 export default function Home() {

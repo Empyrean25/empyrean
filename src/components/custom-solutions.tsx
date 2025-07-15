37e965fac22601b296c8869cab732bf742dd80f9
Empyrean25 <bjorn@empyrean.ph>
2025-07-15 19:50:06 +0800

    Update custom-solutions.tsx

--- src/components/custom-solutions.tsx
+++ src/components/custom-solutions.tsx
@@ -5,8 +5,8 @@ export default function CustomSolutions() {
     <div className="bg-[#494949] text-white py-12 px-8 w-full md:px-16 rounded-lg text-start flex flex-col items-start justify-start">
       <h2 className="text-2xl  font-bold mb-4">NEED A CUSTOM SOLUTION?</h2>
       <p className="max-w-3xl mb-8">
-        Let's talk! Our company is built on fostering quality relationships to observe the <strong>Empyrean Mark</strong>.
-        We'd love to get to know you better so we can assess and evaluate how can we work together and tailor up arrangements personalised for you!
+        Let&rsquo;s talk! Our company is built on fostering quality relationships to observe the <strong>Empyrean Mark</strong>.
+        We&rsquo;d love to get to know you better so we can assess and evaluate how can we work together and tailor up arrangements personalised for you!
       </p>
       <Link
         href="/on-work/contact-us"

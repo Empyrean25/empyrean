9ddc68f3934e8d3872704fcee729c5c330a22de5
Empyrean25 <bjorn@empyrean.ph>
2025-07-18 00:57:25 +0800

    Update content-card.tsx

--- src/components/content-card.tsx
+++ src/components/content-card.tsx
@@ -19,12 +19,14 @@ export function ContentCard({
         <div className="flex items-center gap-4">
           <div className="relative min-w-[60px] h-[60px]">
             <div className="bg-gray-200 rounded-full w-full h-full"></div>
-            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 text-xl font-bold uppercase whitespace-nowrap">
+            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 text-[10px] text-center font-bold uppercase whitespace-normal break-words w-[60px] px-1 leading-tight">
               {title}
             </div>
           </div>
         </div>
-        <p className="text-[14px] md:text-base text-gray-800">{description}</p>
+        <p className="text-[14px] md:text-base text-gray-800 text-justify">
+          {description}
+        </p>
       </div>
     </div>
   );

c8ce5fa2bfbb85ff9a907ee98329a6158f4b70ea
DeihlReyes <219239621+Empyrean25@users.noreply.github.com>
2025-07-22 15:13:34 +0800

    Refactor layout of title in ContentCard component for improved styling and alignment

--- src/components/content-card.tsx
+++ src/components/content-card.tsx
@@ -16,12 +16,12 @@ export function ContentCard({
       className={`border border-gray-300 rounded-lg p-8 md:p-12 ${className}`}
     >
       <div className="flex flex-col md:items-start gap-6">
-        <div className="flex items-center gap-4">
-          <div className="relative min-w-[60px] h-[60px]">
-            <div className="bg-gray-200 rounded-full w-full h-full"></div>
-            <div className="absolute top-1/2 left-1/2 -translate-y-1/2 text-xl font-bold uppercase whitespace-nowrap">
-              {title}
-            </div>
+        <div className="flex relative items-center justify-start gap-4 w-full">
+          <div>
+            <div className="bg-gray-200 rounded-full min-w-[60px] h-[60px]"></div>
+          </div>
+          <div className="absolute left-[20px] md:text-xl font-bold uppercase text-wrap w-full">
+            {title}
           </div>
         </div>
         <p className="text-[14px] md:text-base text-gray-800">{description}</p>

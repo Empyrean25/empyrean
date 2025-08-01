377ade16b8cb49bb645c784ebc045543b6c29d8c
Empyrean25 <bjorn@empyrean.ph>
2025-08-01 16:44:48 +0800

    Update page.tsx

--- src/app/on-work/services/property-owner/page.tsx
+++ src/app/on-work/services/property-owner/page.tsx
@@ -155,12 +155,12 @@ export default function PropertyOwnerPage() {
           keep track of your properties&apos; performance with us. They shall
           be the one responsible in handling your account with us and attend to
           all your inquiries in accessing our Business Network and Professional
-          Circles or any concerns regarding your properties. With Empyrean, you&apos;d have our guarantee that we will reflect our
+          Circles or any concerns regarding your properties. 
+          <br />
+          <br />
+          With Empyrean, you&apos;d have our guarantee that we will reflect our
           core values in all our transactions: we are simple, we are competent,
-          we are accountable, and we are setting a quality standard.
-          <br />
-          <br />
-          We are centralising Real Estate so it doesn&apos;t have to be
+          we are accountable, and we are setting a quality standard. We are centralising Real Estate so it doesn&apos;t have to be
           complicated for you. Your effective Real Estate Solutions, on us.
         </p>
 

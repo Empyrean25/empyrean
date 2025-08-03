f3426b41aeb33b6362235e480c18490fd11f4511
Empyrean25 <bjorn@empyrean.ph>
2025-08-03 17:54:16 +0800

    Update service-card.tsx

--- src/components/service-card.tsx
+++ src/components/service-card.tsx
@@ -7,8 +7,10 @@ export function ServiceCard({ title, description }: ServiceCardProps) {
   return (
     <div className="border border-gray-300 rounded-lg p-8 hover:border-gray-400 transition-colors">
       <div className="flex flex-col items-start justify-center gap-6">
-        <div className="flex relative items-center justify-start gap-4 w-full">
-          <div className="bg-gray-200 rounded-full min-w-[60px] h-[60px]"></div>
+        <div className="flex relative items-center justify-start gap-4 w-full mb-2">
+          <div>
+            <div className="bg-gray-200 rounded-full min-w-[60px] h-[60px]"></div>
+          </div>
           <div className="absolute left-[20px] md:text-xl font-bold uppercase text-wrap w-full">
             {title}
           </div>

152ae5a9e9a12b3e6734d0c0d20fb34862a9e5bf
Empyrean25 <bjorn@empyrean.ph>
2025-08-03 20:20:28 +0800

    Update page.tsx

--- src/app/on-work/services/broker-agent/page.tsx
+++ src/app/on-work/services/broker-agent/page.tsx
@@ -92,7 +92,7 @@ export default function BrokerAgentDeveloperPage() {
             asChild
             className="bg-[#494949] hover:bg-[#494949]/80 text-white rounded-full py-6 px-12"
           >
-            <Link href="/on-work/contact-us">PARTNER WITH US TODAY!!</Link>
+            <Link href="/on-work/contact-us">Become a Broker Partner</Link>
           </Button>
         </div>
       </div>

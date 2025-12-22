a1957e5cbba8c5806afac372ccd119a6cab1adaa
Empyrean25 <bjorn@empyrean.ph>
2025-12-22 08:55:45 +0800

    Update page.tsx

--- src/app/philippinerealestateinventory/page.tsx
+++ src/app/philippinerealestateinventory/page.tsx
@@ -47,8 +47,7 @@ export default function PropertyOwnerPage() {
         >
           <ContentCard title="Check Availability and Schedule a Viewing">
             <>
-              Join our dedicated channel to check on a property&apos;s
-              availability and schedule a viewing. You may also join us as an Official Sales Partner (firms, freelancers, and agents) <strong>for higher commission share!</strong> Contact us for partnership.
+              Please take note of the listing code and join our dedicated channel to check on a property&apos;s availability and schedule a viewing. You may also join us as an Official Sales Partner (firms, freelancers, and agents) <strong>for higher commission share!</strong> Contact us for partnership.
               <div className="flex flex-wrap justify-end gap-2 mt-4">
                 <a
                   href="https://chat.whatsapp.com/C7Pr6gAcFxeH9dM0f6f8lO"

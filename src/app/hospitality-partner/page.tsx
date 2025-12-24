c74d6c0fae01148307af53850620ae3f77fc8b9e
Empyrean25 <bjorn@empyrean.ph>
2025-12-24 17:59:22 +0800

    Update page.tsx

--- src/app/hospitality-partner/page.tsx
+++ src/app/hospitality-partner/page.tsx
@@ -119,9 +119,6 @@ export default function BrokerAgentDeveloperPage() {
         </section>
         
         <p className="text-sm md:text-base text-center mb-16 max-w-4xl mx-auto">
-        As a startup, we are continuously improving all our systems for all our partners. We appreciate your patience and understanding while we work on fully establishing our systems and the company.
-          <br />
-          <br />
           We are very glad to have you with us. Should you have any questions or feedback, please contact your Relationship Manager or reach out through our available channels.
           <br />
           <br />
